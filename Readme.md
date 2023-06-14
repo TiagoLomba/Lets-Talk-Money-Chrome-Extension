92f3d335377d740c4c656253c5fd7cab67ed94bf
Tiago Lomba <lombat27@gmail.com>
2023-06-14 16:47:02 +0100

    Chrome extension created

---
+++ Readme.md
@@ -0,0 +1,47 @@
+## Lets Talk Money
+
+This is a simple and clean Chrome Extension that let's you convert up to 20 different most used currencies!
+
+- Register/Login in API Ninjas 
+- Go to "My Account" and copy your API KEY
+- Paste your API KEY in the exchange.js file (instructions in file)
+- Install the chrome extension on your browser
+    - Go to Chrome Extensions
+    - Enable Developer Mode
+    - Click Load unpacked
+    - Select Folder
+- Click the newly added Let's Talk Money Extension
+- Choose the amount
+- Choose from and to which currency
+- Converted!
+
+Now you can more easily compare prices to your native currency in just a few clicks! 
+
+Currencies
+--
+- EUR (Euro)
+- USD (US Dollar)
+- GBP (British Pound)
+- AED (United Arab Emirates Dirham)
+- AUD (Australian Dollar)
+- BRL (Brazilian Real)
+- CHF (Swiss franc)
+- CZK (Czech Republic Koruna)
+- DKK (Danish Krone)
+- HKD (Hong Kong Dollar)
+- INR (Indian Rupee)
+- JPY (Japanese Yen)
+- MXN (Mexican Peso)
+- NOK (Norwegian Krone)
+- NZD (New Zealand Dollar)
+- PLN (Polish Zloty)
+- SEK (Swedish Krona)
+- SGD (Singapore Dollar)
+- TRY (Turkish Lira)
+--
+
+
+## Screenshots
+
+![App Screenshot](Screenshot1.PNG)
+![App Screenshot](Screenshot2.PNG)
---
+++ exchange.js
@@ -0,0 +1,35 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const amount = document.getElementById('amount')
+    const oldCurrency = document.getElementById('oldCurrency')
+    const newCurrency = document.getElementById('newCurrency')
+    const convert = document.getElementById('convert')
+    const result = document.getElementById('result')
+
+    // INSERT YOUR API KEY FROM API NINJAS HERE https://api-ninjas.com/api/exchangerate
+    const API_KEY="YOUR_API_KEY!"
+    const apiUrl="https://api.api-ninjas.com/v1/convertcurrency?have="
+
+    convert.addEventListener('click', () => {
+        const amountTotal = amount.value;
+        const oldCurrencyTotal = oldCurrency.value;
+        const newCurrencyTotal = newCurrency.value;
+        console.log("Old ", oldCurrencyTotal);
+        const url = apiUrl + oldCurrencyTotal + "&want=" + newCurrencyTotal + "&amount=" + amountTotal;
+        console.log("URL", url)
+
+        fetch(url, {
+            headers: {
+                'X-API-KEY': API_KEY
+            }
+        })
+        .then(response=> response.json())
+        .then(data =>{
+            const resultPrice =  data.new_amount;
+            result.innerHTML = `${amountTotal} ${oldCurrencyTotal} = ${resultPrice.toFixed(2)} ${newCurrencyTotal}`;
+        })
+        .catch(error =>{
+            console.error('Request Failed;', error);
+            result.innerHTML = 'An error occured please try again later.'
+        })
+    })
+})
